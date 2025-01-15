5af18ee94c5853beb44a91b925c43fa0072f1748
bogom1l <bogomil.stoev01@gmail.com>
2025-01-15 14:48:27 +0200

    Updated CloseCommand

--- Library/src/main/java/bg/tu/varna/sit/service/BookService.java
+++ Library/src/main/java/bg/tu/varna/sit/service/BookService.java
@@ -66,6 +66,8 @@ public class BookService {
 
     public void close() {
         System.out.println("File closed.");
+        books = new ArrayList<>();
+        users = new ArrayList<>();
     }
 
     public void save() {
